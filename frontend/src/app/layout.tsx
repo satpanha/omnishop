912cd3e599ba483745a94bdcd8ce0c9d6f496a3e
satpanha <panha.sat618@gmail.com>
2026-06-22 22:54:55 +0700

    add vercel library

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import TelegramProvider from "@/components/TelegramProvider";
 import "@/styles/globals.css";
 
@@ -31,6 +33,8 @@ export default function RootLayout({
       </head>
       <body suppressHydrationWarning>
         <TelegramProvider>{children}</TelegramProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
